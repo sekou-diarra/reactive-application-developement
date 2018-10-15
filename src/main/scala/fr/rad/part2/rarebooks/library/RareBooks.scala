41bccef5e6e92202ca54cf4b1ddef7c428282b43
diarra <dev.diarra@gmail.com>
2018-10-16 00:30:39 +0200

    4.5

--- src/main/scala/fr/rad/part2/rarebooks/library/RareBooks.scala
+++ src/main/scala/fr/rad/part2/rarebooks/library/RareBooks.scala
@@ -1,59 +1,67 @@
 package fr.rad.part2.rarebooks.library
 
-import akka.actor.{Actor, ActorLogging, ActorRef, Props, Stash}
+import akka.actor.{Actor, ActorLogging, Props, Stash}
+import akka.routing.{ActorRefRoutee, RoundRobinRoutingLogic, Router}
 import fr.rad.part2.rarebooks.library.RareBooks.{Close, Open, Report}
 import fr.rad.part2.rarebooks.library.RareBooksProtocol.Msg
+
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.{ MILLISECONDS => Millis, FiniteDuration, Duration }
+import scala.concurrent.duration.{Duration, FiniteDuration, MILLISECONDS => Millis}
 
 
 object RareBooks {
-  case object Close
-  case object Open
-  case object Report
 
   def props: Props = Props(new RareBooks)
+
+  case object Close
+
+  case object Open
+
+  case object Report
 }
 
 
-class RareBooks extends Actor with ActorLogging with Stash{
+class RareBooks extends Actor with ActorLogging with Stash {
 
-  private val openDuration: FiniteDuration = Duration(context.system.settings.config.getDuration("rare-books.open-duration", Millis),Millis)
-  private val closeDuration: FiniteDuration =Duration(context.system.settings.config.getDuration("rare-books.close-duration", Millis),Millis)
-  private val findBookDuration: FiniteDuration =Duration(context.system.settings.config
-    .getDuration("rare-books.librarian.find-book-duration", Millis),Millis)
+  private val openDuration: FiniteDuration = Duration(context.system.settings.config.getDuration("rare-books.open-duration", Millis), Millis)
+  private val closeDuration: FiniteDuration = Duration(context.system.settings.config.getDuration("rare-books.close-duration", Millis), Millis)
+  private val findBookDuration: FiniteDuration = Duration(context.system.settings.config
+    .getDuration("rare-books.librarian.find-book-duration", Millis), Millis)
+  private val nbrOfLibrarians: Int = context.system.settings.config.getInt("rare-books.nbr-of-librarians")
 
-  private val librarian = createLibrarian()
+
+  private val router: Router = createLibrarian()
 
   var requestToday: Int = 0
-  var totalRequests: Int=0
+  var totalRequests: Int = 0
 
 
-  context.system.scheduler.scheduleOnce(openDuration,self,Close)
+  context.system.scheduler.scheduleOnce(openDuration, self, Close)
 
 
   override def receive: Receive = open
 
+
   private def open: Receive = {
     case m: Msg =>
-     requestToday= List(requestToday,1).foldLeft(requestToday)(_+_)
-      librarian forward m
+      router.route(m, sender)
+      requestToday = List(requestToday, 1).foldLeft(requestToday)(_ + _)
+//      librarian forward m
 
     case Close =>
-      context.system.scheduler.scheduleOnce(closeDuration, self,Open)
+      context.system.scheduler.scheduleOnce(closeDuration, self, Open)
       context.become(closed)
 
       self ! Report
   }
 
-
   private def closed: Receive = {
 
     case Report =>
-     requestToday= List(requestToday,1).foldLeft(requestToday)(_+_)
+      requestToday = List(requestToday, 1).foldLeft(requestToday)(_ + _)
 
     case Open =>
-      context.system.scheduler.scheduleOnce(openDuration,self, Close)
+      context.system.scheduler.scheduleOnce(openDuration, self, Close)
 
       unstashAll()
 
@@ -64,7 +72,13 @@ class RareBooks extends Actor with ActorLogging with Stash{
 
   }
 
-  protected def createLibrarian():ActorRef = {
-    context.actorOf(Librarian.props(findBookDuration), "librarian")
+  protected def createLibrarian(): Router = {
+    var cnt: Int = 0
+    val routees : Vector[ActorRefRoutee] = Vector.fill(nbrOfLibrarians) {
+      val r = context.actorOf(Librarian.props(findBookDuration), s"librarian-$cnt")
+      cnt += 1
+      ActorRefRoutee(r)
+    }
+    Router(RoundRobinRoutingLogic(), routees)
   }
 }
